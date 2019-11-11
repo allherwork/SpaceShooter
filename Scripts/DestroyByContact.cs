72b69f483362a29245f4a5bb666f5d3abbce7f71
allherwork <allherwork@users.noreply.github.com>
2019-11-10 21:03:50 -0500

    scripts

---
+++ Scripts/DestroyByContact.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+
+    
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.tag == "Player")
+        {
+
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+        gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        
+        
+    }
+
+        
+   
+    // Start is called before the first frame update
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
